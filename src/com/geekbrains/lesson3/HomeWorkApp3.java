be8205a964de846b7497b7aa5bde21d386ec9dab
Eugene <evg.denisenko@gmail.com>
2022-03-12 23:47:31 +0300

    сделал правки к ДЗ 3

--- src/com/geekbrains/lesson3/HomeWorkApp3.java
+++ src/com/geekbrains/lesson3/HomeWorkApp3.java
@@ -5,6 +5,39 @@ import java.util.Arrays;
 public class HomeWorkApp3 {
     public static void main(String[] args) {
 
+        replaceOneZero();
+        System.out.println("-----");
+
+        createArray(100);
+        System.out.println("-----");
+
+        getDoubleLowSix();
+        System.out.println("-----");
+
+        fillDiagonalSquare(9);
+        System.out.println("-----");
+
+        int[] newArray;
+        newArray = createArrayInit(7, 777);
+        System.out.println(Arrays.toString(newArray));
+        System.out.println("-----");
+
+        findMinMax();
+        System.out.println("-----");
+
+        int[] a = {1, 2, 3, 4, 5, 15};
+        int[] b = {3, 2, 1, 7};
+        int[] err = {1};
+        System.out.println(checkSumLeftRight(a));
+        System.out.println(checkSumLeftRight(b));
+        System.out.println(checkSumLeftRight(err));
+        System.out.println("-----");
+
+        int[] shift = {0, 1, 2, 3, 4, 5, 6, 7};
+        displaceElements(shift, 11);
+    }
+
+    private static void replaceOneZero() {
         int[] array1 = {1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1};
         System.out.println(Arrays.toString(array1));
         for (int i = 0; i < array1.length; i++) {
@@ -15,26 +48,29 @@ public class HomeWorkApp3 {
             }
         }
         System.out.println(Arrays.toString(array1));
-        System.out.println("-----");
+    }
 
-        int[] array2 = new int[100];
-        for (int i = 0; i < array2.length; i++) {
-            array2[i] = i + 1;
+    private static void createArray(int len) {
+        int[] array = new int[len];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
         }
-        System.out.println(Arrays.toString(array2));
-        System.out.println("-----");
+        System.out.println(Arrays.toString(array));
+    }
 
-        int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.println(Arrays.toString(array3));
-        for (int i = 0; i < array3.length; i++) {
-            if (array3[i] < 6) {
-                array3[i] *= 2;
+    private static void getDoubleLowSix() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
             }
         }
-        System.out.println(Arrays.toString(array3));
-        System.out.println("-----");
+        System.out.println(Arrays.toString(array));
+    }
 
-        int[][] square = new int[9][9];
+    private static void fillDiagonalSquare(int len) {
+        int[][] square = new int[len][len];
         for (int i = 0, j = square.length - 1; i < square.length; i++, j--) {
             square[i][i] = 1;
             square[j][i] = 1;
@@ -45,13 +81,17 @@ public class HomeWorkApp3 {
             }
             System.out.println();
         }
-        System.out.println("-----");
+    }
 
-        int[] newArray;
-        newArray = createArray(7, 777);
-        System.out.println(Arrays.toString(newArray));
-        System.out.println("-----");
+    private static int[] createArrayInit(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = initialValue;
+        }
+        return array;
+    }
 
+    private static void findMinMax() {
         int[] numbers = {2121, 3, -5, 0, 876, -88765, 123, -555};
         System.out.println(Arrays.toString(numbers));
         int min = numbers[0];
@@ -64,30 +104,6 @@ public class HomeWorkApp3 {
             }
         }
         System.out.println("min = " + min + ", max = " + max);
-        System.out.println("-----");
-
-        int[] arr = {2, 1, 4, 3, 2, 1, 5, 7, 2, 1, 3, 4, 0};
-        int[] a = {1, 2, 3, 4, 5, 15};
-        int[] b = {3, 2, 1, 7};
-        int[] err = {1};
-        System.out.println(checkSumLeftRight(arr));
-        System.out.println(checkSumLeftRight(a));
-        System.out.println(checkSumLeftRight(b));
-        System.out.println(checkSumLeftRight(err));
-        System.out.println("-----");
-
-        int[] shift = {0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println(Arrays.toString(shift));
-        getShift(shift, 3);
-        getShift(shift, -1);
-    }
-
-    private static int[] createArray(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = initialValue;
-        }
-        return array;
     }
 
     private static boolean checkSumLeftRight(int[] array) {
@@ -111,24 +127,42 @@ public class HomeWorkApp3 {
         return flag;
     }
 
-    private static void getShift(int[] array, int n) {
-        if (n > 0) {
-            for (int i = 0; i < n; i++) {
-                int fin = array[array.length - 1];
-                for (int j = array.length - 1; j > 0; j--) {
-                    array[j] = array[j - 1];
+    private static void displaceElements(int[] array, int num) {
+        System.out.println(Arrays.toString(array));
+        if (num == array.length | num == 0) {
+            System.out.println("Смещения не производится.");
+        } else {
+            if (Math.abs(num) > array.length) {
+                int n = Math.abs(num) % array.length;
+                if (num > 0) {
+                    getShift(array, n);
+                } else {
+                    getShift(array, -n);
                 }
-                array[0] = fin;
-            }
-        } else if (n < 0) {
-            for (int i = 0; i < -n; i++) {
-                int st = array[0];
-                for (int j = 0; j < array.length - 1; j++) {
-                    array[j] = array[j + 1];
-                }
-                array[array.length - 1] = st;
+            } else {
+                getShift(array, num);
             }
         }
         System.out.println(Arrays.toString(array));
+    }
+
+    private static void getShift(int[] array, int n) {
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                int finalElement = array[array.length - 1];
+                for (int j = array.length - 1; j > 0; j--) {
+                    array[j] = array[j - 1];
+                }
+                array[0] = finalElement;
+            }
+        } else if (n < 0) {
+            for (int i = 0; i < -n; i++) {
+                int firstElement = array[0];
+                for (int j = 0; j < array.length - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+                array[array.length - 1] = firstElement;
+            }
+        }
     }
 }
