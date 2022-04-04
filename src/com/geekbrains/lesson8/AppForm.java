f60ce89c6052215cc6790a490eb958f353dc7f16
Eugene <evg.denisenko@gmail.com>
2022-04-04 21:43:09 +0300

    initial commit

---
+++ src/com/geekbrains/lesson8/AppForm.java
@@ -0,0 +1,12 @@
+package com.geekbrains.lesson8;
+
+import javax.swing.*;
+
+public class AppForm extends JFrame {
+    public AppForm(){
+        setTitle("Счетчик");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(500, 200, 300, 200);
+        setVisible(true);
+    }
+}
---
+++ src/com/geekbrains/lesson8/HomeWorkApp8.java
@@ -0,0 +1,7 @@
+package com.geekbrains.lesson8;
+
+public class HomeWorkApp8 {
+    public static void main(String[] args) {
+        AppForm appForm = new AppForm();
+    }
+}
